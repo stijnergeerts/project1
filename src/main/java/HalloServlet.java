a503958b81d6c97d6eea772a5b41e78e48bd997b
Stijn Ergeerts <stijn.ergeerts@student.kdg.be>
2017-02-07 23:51:30 +0100

    jeut

---
+++ src/main/java/HalloServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by StijnErgeerts on 20/09/16.
+ */
+
+@WebServlet("/Hallo")
+public class HalloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
